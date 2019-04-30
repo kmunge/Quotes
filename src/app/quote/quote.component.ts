027bbf793cf6c751bf4890c3c353e66c8f5c47df
kmunge <orokomunge@gmail.com>
2019-04-30 12:39:58 +0300

    creating voting function for up vote and downvote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,29 +10,28 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
     new Quote(1,'DeejayRuxx','Hustle untill you dont have to introduce yourself!','H.J Baba',new Date(2018,3,14)),
-    new Quote(2, 'JackyWanzah','The universe conspires to give you what you lack','Anonymous',new Date(2018,6,9)),
+    new Quote(2,'JackyWanzah','The universe conspires to give you what you lack','Anonymous',new Date(2018,6,9)),
     new Quote(3,'Munge','The no human who is limited, if you focus you go beyond!','Kipchoge',new Date(2018,1,12)),
     new Quote(4,'DeejayRuxx','Hustle untill you dont have to introduce yourself!','H.J Baba',new Date(2018,2,14)),
     new Quote(5,'DeejayRuxx','Hustle untill you dont have to introduce yourself!','H.J Baba',new Date(2018,3,14)),
     new Quote(6,'DeejayRuxx','Hustle untill you dont have to introduce yourself!','H.J Baba',new Date(2018,4,7)),
   ]
 
-  revealDetails(i){
+  revealDetails(i: string | number){
     this.quotes[i].showDetails = !this.quotes[i].showDetails;
   }
  
-  upvote(i) {
-    this.quotes[i].upvotes ++;
+  upvote(i){
+    this.quotes[i].upvotes+=1
   }
-  downvote(i) {
-    this.quotes[i].downvotes ++;
+  downvote(i){
+    this.quotes[i].downvotes+=1
   }
-  
-  delQuote(i) {
-    this.quotes.splice(i, 1)
+  delQuote($event: number) {
+    this.quotes.splice($event, 1)
   }
 
- addnewQuote(quote){
+ addnewQuote(quote: Quote){
     this.quotes.push(quote) 
   } 
   constructor() { }
